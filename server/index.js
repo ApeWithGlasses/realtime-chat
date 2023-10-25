aa1d1cb5d8a977e4162c7e96a352994bb222f3c7
ApeWithGlasses <wcgiraldo0@gmail.com>
2023-10-25 11:47:44 -0500

    feat: :sparkles: Chat connected to database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
 import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
 
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
-import { createClient } from '@libsql/client/.';
+
+dotenv.config();
 
 const port = process.env.PORT ?? 3000;
 
@@ -15,19 +17,52 @@ const io = new Server(server, {
 });
 
 const db = createClient({
-    url: ''
+    url: "libsql://many-flamebird-apewithglasses.turso.io",
+    authToken: process.env.DB_TOKEN
 });
 
-io.on('connection', (socket) => {
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT NOT NULL
+    )
+`);
+
+io.on('connection', async (socket) => {
     console.log('a user has connected');
 
     socket.on('disconnect', () => {
         console.log('a user has disconnected');
     });
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('chat message', async (msg) => {
+        let result
+        try {
+            result = await db.execute({
+                sql: 'INSERT INTO messages (content) VALUES (:msg)',
+                args: { msg }
+            });
+        } catch (e) {
+            console.error(e);
+        }
+
+        io.emit('chat message', msg, result.lastInsertRowid.toString());
     });
+
+    if (!socket.recovered) {
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            });
+
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString());
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
 });
 
 app.use(logger('dev'));
